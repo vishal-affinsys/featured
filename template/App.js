17968e560e0b77de8acc18d34246ea8c2fa03c9b
Vishal Singh <vishal.singh@affinsys.com>
2022-12-06 12:08:56 +0530

    initial commit

---
+++ template/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+import {HomeScreen} from './Screens';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <HomeScreen />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
